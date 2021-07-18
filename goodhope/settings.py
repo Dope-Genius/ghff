ba8e3a534f5bbf067bf133327fa3dffd58d41632
Samuel Jarvis <samueladeyemi244@gmail.com>
2021-07-18 14:13:14 +0100

    aws region

--- goodhope/settings.py
+++ goodhope/settings.py
@@ -136,6 +136,7 @@ AWS_DEFAULT_ACL = None
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400',}
 AWS_S3_FILE_OVERWRITE = False
+AWS_S3_REGION_NAME = 'eu-west-3'
 
 AWS_LOCATION = 'static'
 
